40a8604a257a2ac711a5558b08f23213c7e16ec6
Max Kruse <xam.esurk@gmail.com>
2026-04-17 20:49:20 +0200

    assetlink json verschoen

--- scripts/copy-landing.js
+++ scripts/copy-landing.js
@@ -8,8 +8,17 @@ function copyLanding(srcDir, destDir) {
   fs.cpSync(srcDir, destDir, { recursive: true });
 }
 
+function copyWellKnown() {
+  const src = 'well-known';
+  const dest = 'dist/.well-known';
+  if (fs.existsSync(src)) {
+    fs.cpSync(src, dest, { recursive: true });
+  }
+}
+
 if (import.meta.url === pathToFileURL(process.argv[1]).href) {
   copyLanding('landing', 'dist/landing');
+  copyWellKnown();
 }
 
 export { copyLanding };
